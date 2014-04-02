e1958f6e46b7e3539178f0d1a79c78079440cd34
Vinney K <vinneyk@live.com>
2014-04-01 23:53:58 -0600

    Added Pondtoon library and test projects.
    Includes WaveformHelper and two failing tests.

---
+++ Pondtoon/src/Pondtoon.Tests/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pondtoon.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void SineWaveContainsExpectedCountOfPoints()
+        {
+            // arrange
+            const int sampleRate = 44100;
+            const int frequency = 10000;
+            const int cycles = 100;
+            var systemUnderTest = new WaveformHelper();
+
+            // act
+            var result = systemUnderTest.MakeSine(sampleRate, frequency, cycles);
+
+            // assert
+            Assert.Equal(441, result.Points.Length);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            // arrange
+            const int sampleRate = 44100;
+            const int frequency = 10000;
+            const int cycles = 441;
+            var systemUnderTest = new WaveformHelper();
+            var expected = SineWaveCalculator.ComputePoint(127, frequency, .01);
+
+            // act
+            var result = systemUnderTest.MakeSine(sampleRate, frequency, cycles);
+
+            // assert
+            Assert.Equal(expected, result.Points.Last());
+        }
+
+        internal static class SineWaveCalculator
+        {
+            internal static double ComputePoint(int amplitude, double frequency, double seconds, double phaseShift = 0)
+            {
+                return amplitude*(((2*Math.PI)*frequency*seconds)+phaseShift);
+            }
+        }
+    }
+}
---
+++ Pondtoon/src/Pondtoon/WaveformHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pondtoon
+{
+    public class WaveformHelper
+    {
+        private const int ZeroCrossover = 128;
+        private const int EightBitFriendlyAmplitude = 127;
+        private const double Tau = Math.PI*2;
+
+        public WaveForm MakeSine(int sampleRate, double frequency, int cycles, int amplitude = EightBitFriendlyAmplitude)
+        {
+            var radians = 0d; 
+            var delta = Tau/(sampleRate/frequency); 
+            var points = new List<double>();
+            var requiredRadians = cycles*Tau;
+
+            while (radians <= requiredRadians)
+            {
+                points.Add(Math.Sin(radians)*amplitude);
+                radians += delta;
+            }
+
+            return new WaveForm
+            {
+                Amplitude = amplitude,
+                Frequency = frequency,
+                Points = points.ToArray(),
+                SampleRate = sampleRate,
+            };
+        }
+
+        public void ShiftPointsFor8Bit()
+        {
+            //todo: add 128 to each point to prevent negatives for 8bit friendly numbers
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct WaveForm
+    {
+        public double[] Points { get; set; }
+        public int Amplitude { get; set; }
+        public double Frequency { get; set; }
+        public int SampleRate { get; set; }
+    }
+}
